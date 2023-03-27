661bd3f34dd08a894275543ad93c0b6650ca6b1a
Su Yue <glass@fydeos.io>
2023-03-27 13:36:16 +0000

    Copy phicomm n1 patches from RuralHunter/CoreELEC Great thanks to https://github.com/RuralHunter/CoreELEC.
    Signed-off-by: Su Yue <glass@fydeos.io>

--- projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot/package.mk
+++ projects/Amlogic-ce/devices/Amlogic-ng/packages/u-boot/package.mk
@@ -43,6 +43,16 @@ makeinstall_target() {
     unset PKG_UBOOTBIN
     unset PKG_CHAINUBOOTBIN
     find_file_path bootloader/${PKG_SUBDEVICE}_boot.ini && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
+
+    # Phicommon_N1
+    for src in $INSTALL/usr/share/bootloader/*_boot.ini ; do
+      sed -e "s/@BOOT_LABEL@/$DISTRO_BOOTLABEL/g" \
+          -e "s/@DISK_LABEL@/$DISTRO_DISKLABEL/g" \
+          -i "$src"
+      sed -e "s/@DTB_NAME@/$DEFAULT_DTB_NAME/g" \
+          -i "$src"
+    done
+    
     if [ "${PKG_SUBDEVICE:0:9}" = "Odroid_N2" -o "${PKG_SUBDEVICE}" = "Odroid_C4" -o "${PKG_SUBDEVICE}" = "Odroid_HC4" -o "${PKG_SUBDEVICE:0:10}" = "Radxa_Zero" ]; then
       if [ "${PKG_SUBDEVICE}" != "Odroid_HC4" ]; then
         PKG_UBOOTBIN=$(get_build_dir u-boot-${PKG_SUBDEVICE})/sd_fuse/u-boot.bin.sd.bin
@@ -65,8 +75,4 @@ makeinstall_target() {
   find_file_path bootloader/config.ini && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
     sed -e "s/@PROJECT@/${PKG_CANUPDATE}/g" \
         -i $INSTALL/usr/share/bootloader/canupdate.sh
-  # Copy Hardkernel boot logo
-  find_file_path splash/${DEVICE}/hk-boot-logo-1080.bmp.gz && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
-	# Copy Radxa boot logo
-  find_file_path splash/${DEVICE}/radxa-boot-logo-1080.bmp.gz && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
 }
